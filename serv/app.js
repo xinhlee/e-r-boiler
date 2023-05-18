e76e8bd6f08d2969e87fe18e2a60df9b373960d3
xinhlee <xinhong9299@gmail.com>
2023-05-18 17:11:04 +0800

    first commit

---
+++ serv/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("build"));
+
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
+app.post("/sendName", (req, res) => {
+  console.log("NAME RECEIVED");
+  console.log("saved to databse");
+
+  res.send("NAME SENT");
+});
+
+app.listen(5000, () => {
+  console.log("Example app listening on port 3000!");
+});
